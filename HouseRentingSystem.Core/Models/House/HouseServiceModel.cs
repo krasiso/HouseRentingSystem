b70488e904b39626816f9bb408f76fb1293b643d
Krasimir Nikolov <kraschonikolov@gmail.com>
2022-11-17 14:59:53 +0200

    AllHouses implemented

---
+++ HouseRentingSystem.Core/Models/House/HouseServiceModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HouseRentingSystem.Core.Models.House
+{
+    public class HouseServiceModel
+    {
+        public int Id { get; init; }
+
+        public string Title { get; init; } = null!;
+
+        public string Address { get; init; } = null!;
+
+        [Display(Name = "Image URL")]
+        public string ImageUrl { get; init; } = null!;
+
+        [Display(Name = "Price per month")]
+        [Range(0.00, 2000.00, ErrorMessage = "Price per month must be positive number and less than {2} leva")]
+        public decimal PricePerMonth { get; init; }
+
+        [Display(Name = "Is Rented")]
+        public bool IsRented { get; init; }
+    }
+}
